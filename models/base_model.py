93c03f972a4c5dd79b061c4a1cf3b0a83ae6d693
micaelapicco <micaelaabril421@gmail.com>
2023-08-01 11:35:32 -0700

    sql connection

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,15 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
