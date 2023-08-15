2f186d5548237ea8e1a6224a9349fad7140a488a
micaelapicco <micaelaabril421@gmail.com>
2023-08-15 12:10:13 -0700

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python3
-"""script that starts a Flask web application"""
+#!/urs/bin/python3
+"""Task 0:
+Say hello and connect port
+"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """when route is / the display is Hello HBNB"""
+    """say hello"""
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
