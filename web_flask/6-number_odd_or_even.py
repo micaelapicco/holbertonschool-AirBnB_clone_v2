f885a5838f962b0684a133e0ba618b234a9ea243
micaelapicco <micaelaabril421@gmail.com>
2023-08-16 08:54:45 -0700

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,5 +46,12 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """display n template if n is an integer and say if even or odd"""
+    type = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, type=type)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
