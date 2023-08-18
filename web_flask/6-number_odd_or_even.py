89977dfca3bdd5a5b1f8bd386397212ced2d84e8
micaelapicco <micaelaabril421@gmail.com>
2023-08-18 11:19:28 -0700

    task 8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Task 5:
+"""Task 6:
 Say hello and connect port
 """
 from flask import Flask, render_template
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Task 8:
+script that starts a Flask web application:
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_sql(remove):
+    """remove the current SQLA session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Display states"""
+    all_states = storage.all(State).values()
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
